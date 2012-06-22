ccf5fa4f1c85d446d5a09cad0911560ea46de69c
George <zephon.pierce@gmail.com>
2012-06-22 00:32:01 -0400

    Barebone Rails app

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '941813e6c416b3a51a254cf23dec28130ad369ee0fbe6b60cb869692b92b6b5da97cfdbd3acdd19ab7951d0ea8723de0eb25c87acda13eb0aa071c486f47780e'
